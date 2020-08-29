66ffe63dc28fbbf369a4aad9450d27e3de6ad38e
Cédric <github@hidemyhome.ovh>
2020-08-29 18:07:26 +0200

    Ajout: fonction décomposée en 2 mail: un pour le destinataire (proprio du site afin qu'il recoive le message du formulaire + un AR au client

--- sendMail.js
+++ sendMail.js
@@ -1,8 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const sender = (mail, name, firstname, message) => {
-  //const subject = "De la part de " + firstname + " " + name + " sur votre CV";
-
+const sender = (mail, subject, message, messageHTML) => {
   return new Promise((resolve, reject) => {
     async function main() {
       // Création du transporteur
@@ -20,9 +18,9 @@ const sender = (mail, name, firstname, message) => {
       await transporter.sendMail({
         from: process.env.CV_Mail_From, // Expéditeur
         to: mail, // Destinataires (si plusieurs les séparés par des ,)
-        subject: "De la part de " + firstname + " " + name + " sur votre CV", // Sujet du mail
+        subject: subject, // Sujet du mail
         text: message, // Texte sans HTML
-        html: message, // Texte avec du html
+        html: messageHTML, // Texte avec du html
       });
       resolve("Message envoyé");
     }
@@ -32,9 +30,33 @@ const sender = (mail, name, firstname, message) => {
 };
 
 module.exports.sendMeAMail = (req, res, next) => {
+  // Récupération des variables contenues dans le body
   const { mail, name, firstname, message } = req.body;
 
-  sender(mail, name, firstname, message)
+  // Création du sujet du mail
+  let subject = "De la part de " + firstname + " " + name + " sur votre CV";
+
+  // 1ère étape, je m'envoie un mail avec les informations contenues dans le formulaire
+  sender(process.env.CV_Mail_To, subject, message, message)
     .then((resultat) => res.status(200).send(resultat))
     .catch((err) => res.status(400).send(err));
+
+  // Création du sujet du mail
+  subject = "Merci " + firstname + " pour votre message!";
+
+  // Création du message du mail
+  const messageAutoWithoutHTML =
+    "Cher/Chère " +
+    firstname +
+    "\n\nTout d'abord, merci pour votre message sur mon CV Web. \nJe vais le lire attentivement, je ne manquerai pas de vous faire un retour dès que possible! \n\nCordialement, \nCédric";
+
+  const messageAutoWithHTML =
+    "Cher/Chère <strong>" +
+    firstname +
+    "</strong><br><br>Tout d'abord, merci pour votre message sur mon CV Web. <br>Je vais le lire attentivement, je ne manquerai pas de vous faire un retour dès que possible! <br><br>Cordialement, <br>Cédric";
+
+  //2e etape, j'envoie une mail type accusé de récéption au client
+  sender(mail, subject, messageAutoWithoutHTML, messageAutoWithHTML)
+    .then((resultat) => console.log(resultat))
+    .catch((err) => console.log(err));
 };
