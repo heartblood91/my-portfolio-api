245eff5f2adaed8aeed93b69c5471a8d511cd4bd
Cédric <github@hidemyhome.ovh>
2020-09-09 12:31:36 +0200

    Ajout: fonction permettant de transformer un message de texte à html + mail vers ma boite plus complet avec toutes les informations du formulaire + changement de noms de variables

--- sendMail.js
+++ sendMail.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const sender = (mail, subject, message, messageHTML) => {
+const sender = (email, subject, message, messageHTML) => {
   return new Promise((resolve, reject) => {
     async function main() {
       // Création du transporteur
@@ -14,11 +14,11 @@ const sender = (mail, subject, message, messageHTML) => {
         },
       });
 
-      // send mail with defined transport object
+      // send email with defined transport object
       await transporter.sendMail({
         from: process.env.CV_Mail_From, // Expéditeur
-        to: mail, // Destinataires (si plusieurs les séparés par des ,)
-        subject: subject, // Sujet du mail
+        to: email, // Destinataires (si plusieurs les séparés par des ,)
+        subject: subject, // Sujet du email
         text: message, // Texte sans HTML
         html: messageHTML, // Texte avec du html
       });
@@ -30,33 +30,56 @@ const sender = (mail, subject, message, messageHTML) => {
 };
 
 module.exports.sendMeAMail = (req, res, next) => {
+  // Etape 0: Je récupère les informations
   // Récupération des variables contenues dans le body
-  const { mail, name, firstname, message } = req.body;
+  const { email, name, firstname, message, tel } = req.body;
 
-  // Création du sujet du mail
+  // Etape 1: Je prépare le 1er email (la transmission du message du client)
+  // Création du sujet du email
   let subject = "De la part de " + firstname + " " + name + " sur votre CV";
 
-  // 1ère étape, je m'envoie un mail avec les informations contenues dans le formulaire
-  sender(process.env.CV_Mail_To, subject, message, message)
+  // Corps du email
+  let messageAutoWithoutHTML =
+    "Cédric, \n\nJe vous fais parvenir un message avec les informations suivantes depuis votre CV en ligne: \n" +
+    "\nPrénom: " +
+    firstname +
+    "\nNom: " +
+    name +
+    "\nEmail: " +
+    email +
+    "\nTéléphone: " +
+    tel +
+    "\n\nMessage:\n\n" +
+    message;
+
+  //Transforme le texte simple en texte html
+  let messageAutoWithHTML = messageAutoWithoutHTML.replace(/\n/g, "<br>");
+
+  // 1ère étape, je m'envoie un email avec les informations contenues dans le formulaire
+  sender(
+    process.env.CV_Mail_To,
+    subject,
+    messageAutoWithoutHTML,
+    messageAutoWithHTML
+  )
     .then((resultat) => res.status(200).send(resultat))
     .catch((err) => res.status(400).send(err));
 
-  // Création du sujet du mail
+  // Etape 2: Je prépare le second email (accusé récéption pour le client)
+  // Création du sujet du email
   subject = "Merci " + firstname + " pour votre message!";
 
-  // Création du message du mail
-  const messageAutoWithoutHTML =
+  // Création du message du email
+  messageAutoWithoutHTML =
     "Cher/Chère " +
     firstname +
-    "\n\nTout d'abord, merci pour votre message sur mon CV Web. \nJe vais le lire attentivement, je ne manquerai pas de vous faire un retour dès que possible! \n\nCordialement, \nCédric";
+    "\n\nTout d'abord, merci pour votre message sur mon CV Web. \n\nJe vais le lire attentivement, je ne manquerai pas de vous faire un retour dès que possible! \n\nCordialement, \nCédric";
 
-  const messageAutoWithHTML =
-    "Cher/Chère <strong>" +
-    firstname +
-    "</strong><br><br>Tout d'abord, merci pour votre message sur mon CV Web. <br>Je vais le lire attentivement, je ne manquerai pas de vous faire un retour dès que possible! <br><br>Cordialement, <br>Cédric";
+  //Transforme le texte simple en texte html
+  messageAutoWithHTML = messageAutoWithoutHTML.replace(/\n/g, "<br>");
 
-  //2e etape, j'envoie une mail type accusé de récéption au client
-  sender(mail, subject, messageAutoWithoutHTML, messageAutoWithHTML)
+  //2e etape, j'envoie une email type accusé de récéption au client
+  sender(email, subject, messageAutoWithoutHTML, messageAutoWithHTML)
     .then((resultat) => console.log(resultat))
     .catch((err) => console.log(err));
 };
